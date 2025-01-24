898e65ac325e4f7b817acaa801cb601f3aad495d
Taufik Hidayat <tfkhdyt@proton.me>
2025-01-24 21:23:52 +0700

    chore: setup prisma

--- flake.nix
+++ flake.nix
@@ -19,7 +19,13 @@
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ bun ];
+          buildInputs = with pkgs; [ bun prisma ];
+          shellHook = with pkgs; ''
+            export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
+            export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+            export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+            export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+          '';
         };
       }
     );
