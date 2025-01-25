f29b8378b300ef287cb1bf5db8009ebecbba3752
Taufik Hidayat <tfkhdyt@proton.me>
2025-01-25 20:12:09 +0700

    feat(db): add initial database schema and update project configuration
    Add initial Prisma migration files, including `User` and `Message` tables, and lock the migration provider. Update `.gitignore` to exclude `prisma/dev.db-journal` and enhance `package.json` with a `start` script for deploying migrations and running the application.

---
+++ prisma/migrations/20250125130704_fresh_migrate/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Message_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
